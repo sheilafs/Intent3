c329617b661a81220f68e0020a16f68a975a8d43
sheila <sheilafatanihzasafitri@gmail.com>
2016-11-03 13:47:04 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -0,0 +1,20 @@
+package id.sch.smktelkom_mlg.learn.intent3;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
