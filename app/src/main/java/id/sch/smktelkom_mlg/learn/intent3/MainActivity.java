979ed0d0c248c9341ac643cc5491b06483586df8
sheila <sheilafatanihzasafitri@gmail.com>
2016-11-03 13:49:15 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -16,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.imageViewPhone).setOnClickListener((new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                dialPhoneNumber("0341712500");
+            }
+        }));
+    }
+
+    public void dialPhoneNumber(String phoneNumber)
+    {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:"+phoneNumber));
+        if(intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
 }
